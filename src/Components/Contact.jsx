88700f462bbbd7d22b8962abbe3c393725ea3f9a
bala-077 <hariharan101311@gmail.com>
2024-09-28 09:10:23 +0530

    some minimal changes and popup indication

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,12 +1,16 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import { ThemeContext } from '../ThemeProvider';
 import home from '../Images/home.png';
 import phone from '../Images/phone.png';
 import message from '../Images/message.png';
 
 const Contact = () => {
-    // For the contact info display
-    const [contact] = useState([
+    const [messageData, setMessageData] = useState({ text: '', type: 'success' });
+    const [showMessage, setShowMessage] = useState(false);
+    const timeoutRef = useRef(null);
+
+    // Contact information data
+    const contactInfo = [
         {
             image: home,
             title: 'Location',
@@ -25,12 +29,12 @@ const Contact = () => {
             details: 'balahariharan.mca@gmail.com',
             link: 'mailto:balahariharan.mca@gmail.com'
         }
-    ]);
+    ];
 
-    // For theme context
+    // Accessing the current theme
     const { theme } = useContext(ThemeContext);
 
-    // For handling the form data
+    // Form state management
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -38,7 +42,7 @@ const Contact = () => {
         message: ''
     });
 
-    // Handling input changes
+    // Handle form input changes
     const handleChange = (e) => {
         setFormData({
             ...formData,
@@ -46,9 +50,9 @@ const Contact = () => {
         });
     };
 
-    // Handling form submission
+    // Handle form submission
     const handleSubmit = async (e) => {
-        e.preventDefault(); // Prevent page reload
+        e.preventDefault();
         try {
             const response = await fetch('https://portfolio-backend-74o1.onrender.com/api/contact', {
                 method: 'POST',
@@ -60,38 +64,46 @@ const Contact = () => {
 
             const result = await response.json();
             if (response.ok) {
-                alert('Your message has been sent successfully!');
-                // Clear form after submission
-                setFormData({
-                    name: '',
-                    email: '',
-                    phone: '',
-                    message: ''
-                });
+                setMessageData({ text: 'Your message has been sent successfully!', type: 'success' });
+                setShowMessage(true);
+                setFormData({ name: '', email: '', phone: '', message: '' });
             } else {
-                alert('Failed to send message, please try again.');
+                setMessageData({ text: 'Failed to send message, please try again.', type: 'error' });
+                setShowMessage(true);
             }
         } catch (error) {
-            alert('An error occurred. Please try again later.');
+            setMessageData({ text: 'An error occurred. Please try again later.', type: 'error' });
+            setShowMessage(true);
         }
     };
 
+    // Manage popup auto-hide
+    useEffect(() => {
+        if (showMessage) {
+            timeoutRef.current = setTimeout(() => setShowMessage(false), 3000);
+        }
+        return () => clearTimeout(timeoutRef.current);
+    }, [showMessage]);
+
     return (
-        <div className={`w-full flex items-center justify-center ${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-100'} md:px-10 py-10`}>
-            <div className={`max-w-4xl w-full bg-white dark:bg-gray-900 p-8 rounded-lg shadow-lg transition-all duration-500`}>
+        <div className={`w-full flex items-center justify-center ${theme === 'dark' ? 'bg-gray-800' : 'bg-gray-100'} px-0 py-10 relative ,md md:px-4`}>
+            <div className={`max-w-4xl w-full bg-white dark:bg-gray-900 p-8 rounded-lg shadow-lg`}>
+                {/* Heading */}
                 <div className="text-center mb-8">
                     <h1 className={`text-2xl mb-6 ${theme === 'dark' ? 'text-white' : 'text-[#003135]'} font-medium underline md:text-3xl`}>
                         Contact
                     </h1>
                 </div>
+
+                {/* Content Grid */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                    {/* Contact Details Section */}
+                    {/* Contact Details */}
                     <div className="space-y-6">
-                        {contact.map((item, index) => (
+                        {contactInfo.map((item, index) => (
                             <div key={index} className="flex items-start border-b pb-4">
                                 <img src={item.image} alt={`${item.title} Icon`} className="w-8 h-8 mr-4" />
-                                <div className="flex-1">
-                                    <h1 className={`font-semibold text-lg ${theme === 'dark' ? 'text-white' : 'text-gray-700'}`}>{item.title}</h1>
+                                <div>
+                                    <h2 className={`font-semibold text-lg ${theme === 'dark' ? 'text-white' : 'text-gray-700'}`}>{item.title}</h2>
                                     <a
                                         href={item.link}
                                         target="_blank"
@@ -105,9 +117,10 @@ const Contact = () => {
                         ))}
                     </div>
 
-                    {/* Contact Form Section */}
+                    {/* Contact Form */}
                     <div className="space-y-4">
                         <form onSubmit={handleSubmit} className="space-y-4">
+                            {/* Name Field */}
                             <div>
                                 <label htmlFor="name" className={`block text-sm font-medium ${theme === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
                                     Name:
@@ -123,6 +136,8 @@ const Contact = () => {
                                     required
                                 />
                             </div>
+
+                            {/* Email Field */}
                             <div>
                                 <label htmlFor="email" className={`block text-sm font-medium ${theme === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
                                     Email:
@@ -138,6 +153,8 @@ const Contact = () => {
                                     required
                                 />
                             </div>
+
+                            {/* Phone Field */}
                             <div>
                                 <label htmlFor="phone" className={`block text-sm font-medium ${theme === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
                                     Phone:
@@ -153,6 +170,8 @@ const Contact = () => {
                                     required
                                 />
                             </div>
+
+                            {/* Message Field */}
                             <div>
                                 <label htmlFor="message" className={`block text-sm font-medium ${theme === 'dark' ? 'text-gray-300' : 'text-gray-700'}`}>
                                     Message:
@@ -162,16 +181,18 @@ const Contact = () => {
                                     id="message"
                                     value={formData.message}
                                     onChange={handleChange}
-                                    cols="20"
                                     rows="5"
                                     className={`w-full mt-1 p-2 border ${theme === 'dark' ? 'border-gray-600' : 'border-gray-300'} rounded-md focus:ring-blue-500 focus:border-blue-500`}
                                     required
+                                    autoComplete='off'
                                 ></textarea>
                             </div>
+
+                            {/* Submit Button */}
                             <div>
                                 <button
                                     type="submit"
-                                    className={`px-5 py-2 text-white rounded-lg hover:bg-blue-700 transition-colors ${theme === 'dark' ? 'bg-blue-600' : 'bg-blue-500'}`}
+                                    className={`w-full px-5 py-2 text-white rounded-lg hover:bg-blue-700 transition-colors ${theme === 'dark' ? 'bg-blue-600' : 'bg-blue-500'}`}
                                 >
                                     Submit
                                 </button>
@@ -180,8 +201,62 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
+
+            {/* Centered Popup */}
+            {showMessage && (
+                <div
+                    className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+                    role="alert"
+                    aria-live="assertive"
+                >
+                    <div
+                        className={`max-w-sm w-full bg-${messageData.type === 'success' ? 'green' : 'red'}-500 text-white px-6 py-4 rounded-lg shadow-lg transform transition-all duration-300 
+                        ${showMessage ? 'opacity-100 scale-100' : 'opacity-0 scale-95'}
+                        `}
+                    >
+                        <div className="flex items-center">
+                            {/* Success or Error Icon */}
+                            {messageData.type === 'success' ? (
+                                <svg
+                                    className="w-6 h-6 mr-2"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M5 13l4 4L19 7"
+                                    ></path>
+                                </svg>
+                            ) : (
+                                <svg
+                                    className="w-6 h-6 mr-2"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    aria-hidden="true"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    ></path>
+                                </svg>
+                            )}
+                            <span>{messageData.text}</span>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
+
 };
 
 export default Contact;
